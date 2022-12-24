9cbdeb4b8fdfd4e0e34447cd25dcc8a4d72e98a0
JunyTM <20010784@st.phenikaa-uni.edu.com>
2022-12-24 17:34:10 +0700

    Update SignUp screen

--- src/screens/AuthScreen/SignUp.js
+++ src/screens/AuthScreen/SignUp.js
@@ -1,14 +1,11 @@
 import { TouchableOpacity, TextInput, Image, View, Text } from "react-native";
 import React, { useState } from "react";
 import { Icon, Button } from "@rneui/themed";
-import DatePicker from "react-native-date-picker";
-import dayjs from "dayjs";
+
+import UserInfo from "../../components/Profile/UserInfo";
+import ProfileInfo from "../../components/Profile/ProfileInfo";
 
 export default function SignUp({ navigation }) {
-  const [showPass, SetShowPass] = useState(true);
-  const [open, setOpen] = useState(false);
-  const [birthday, setBirthday] = useState(new Date());
-
   return (
     <View
       style={{
@@ -29,72 +26,40 @@ export default function SignUp({ navigation }) {
       </View>
 
       <View className="w-full h-full bg-teal-50">
-        {/* Profile name */}
-        <Text className="text-base ml-6 mt-8">Họ và tên:</Text>
-        <View className="items-center ">
-          <TouchableOpacity className="w-96 h-12 bg-zinc-50 items-center justify-center border-2 border-slate-300">
-            <TextInput
-              placeholder="Họ và tên của bạn"
-              className="pl-4 pr-6 w-full h-8 text-lg "
-              onBlur={(e) => {
-                e.preventDefault();
-                console.log(e.target.value);
-              }}
-            ></TextInput>
-          </TouchableOpacity>
+        <ProfileInfo />
+        <UserInfo />
+
+        <View className="items-center">
+          <Button
+            title="Đăng ký"
+            buttonStyle={{
+              backgroundColor: "#60A5FA",
+              borderWidth: 2,
+              borderColor: "white",
+              borderRadius: 40,
+            }}
+            containerStyle={{
+              width: 300,
+              marginTop: "15%",
+              marginBottom: 25,
+              marginHorizontal: 50,
+              marginVertical: 10,
+            }}
+            // onPress={onLogin}
+            onPress={() => {
+              alert("Đăng ký thành công");
+            }}
+          />
         </View>
 
-        {/* Email */}
-        <Text className="text-base ml-6 mt-4">Email:</Text>
-        <View className="items-center ">
-          <TouchableOpacity className="w-96 h-12 bg-zinc-50 items-center justify-center border-2 border-slate-300">
-            <TextInput
-              placeholder="Gmail của bạn"
-              className="pl-4 pr-6 w-full h-8 text-lg "
-              onBlur={(e) => {
-                e.preventDefault();
-                console.log(e.target.value);
-              }}
-            ></TextInput>
+        {/* Back to login screen */}
+        <View className="mt-8 mr-6 flex-row justify-end items-end">
+          <Text className="text-base">Đã có tài khoản, </Text>
+          <TouchableOpacity onPress={() => {
+            navigation.navigate("Login");
+          }}>
+            <Text className="text-base text-blue-800">Đăng nhập</Text>
           </TouchableOpacity>
-        </View>
-
-        {/* user acount */}
-        <View className="w-full flex-row">
-          <View className="w-3/5 h-24 bg-red-100">
-            <Text className="text-base ml-6 mt-3">Tài khoản:</Text>
-            <View className="ml-5">
-              <TouchableOpacity className="w-48 h-12 bg-zinc-50 items-center justify-center border-2 border-slate-300">
-                <TextInput
-                  placeholder="Họ và tên của bạn"
-                  className="pl-4 pr-6 w-full h-8 text-lg "
-                  onBlur={(e) => {
-                    e.preventDefault();
-                    console.log(e.target.value);
-                  }}
-                ></TextInput>
-              </TouchableOpacity>
-            </View>
-          </View>
-
-          <View className="w-2/5 h-24 bg-blue-100">
-            <Text className="text-base ml-6 mt-3">Ngày sinh:</Text>
-
-            <DatePicker
-              modal
-              open={open}
-              date={birthday}
-              mode="date"
-              locale="vi"
-              onConfirm={(date) => {
-                setOpen(false);
-                setBirthday(date);
-              }}
-              onCancel={() => {
-                setOpen(false);
-              }}
-            />
-          </View>
         </View>
       </View>
     </View>
